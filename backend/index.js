48e97939f5fdbe02af5fbb29751eb88bf9556643
Hiteshydv <113702779+Hiteshydv@users.noreply.github.com>
2023-12-06 23:56:46 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,12 @@
+import express from "express";
+import authRoute from "./routes/auth.js"
 
+const app=express()
+
+app.use(express.json());
+
+app.use("/api/auths",authRoute)
+
+app.listen(8800,()=>{
+    console.log("connected to backend")
+})
